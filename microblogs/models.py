469f99617926ffbd4c2368bfa891be3536d9f82c
Aniad <aniad.hossain@outlook.com>
2023-11-08 14:53:55 +0000

    add url path to lead to the sign up html page

--- microblogs/models.py
+++ microblogs/models.py
@@ -2,11 +2,13 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+
+
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20,
-                                unique=True)
+    username = models.CharField(max_length=20,unique=True)
     email = models.EmailField(unique=True,blank=False)
     first_name = models.CharField(max_length=50,blank=False)
     last_name = models.CharField(max_length=50,blank=False)
--- microblogs/views.py
+++ microblogs/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def sign_up(request):
+    return render(request, 'sign_up.html')
+
     
 
